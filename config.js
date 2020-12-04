8cd34ae07555045ccd724ba3d8ace165d19ed8c4
harshityta <75496742+harshityta@users.noreply.github.com>
2020-12-04 23:11:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `+`;//your prefix for bot
-exports.Token = `NzgzNzI0NTQ5NzI4NTAxNzkx.X8e6XQ.tA-tFvYAZYnrshywwdLJN2V1xMY`;//your token 
+exports.Token = ``;//your token 
 exports.Color = `RANDOM`;//color   of embed
